63c3ecfe14563ef7d3da3522434e9bf5d40637df
Pabo Bernando <pabobernando69@gmail.com>
2023-10-24 14:13:49 +0700

    update Pengurus

--- src/store/pengurusData.js
+++ src/store/pengurusData.js
@@ -28,133 +28,145 @@ import Koni22 from '../assets/22.jpeg'
 import Koni23 from '../assets/23.jpeg'
 import Koni24 from '../assets/24.jpeg'
 import Koni25 from '../assets/25.jpeg'
+import Koni26 from '../assets/26.jpeg'
+import Koni27 from '../assets/27.jpeg'
 
 
 export const pengurusData = [
+  {
+    name: 'Kusdira, BA',
+    role: 'Ketua Umum Koni Kulon Progo',
+    image: Koni16,
+  },
+  {
+    name: 'H. Margono, S.Pd, Jas',
+    role: 'Wakil Ketua Umum I',
+    image: Koni6,
+  },
+  {
+    name: 'Drs. Sujiran',
+    role: 'Wakil Ketua Koni II',
+    image: Koni22,
+  },
+  {
+    name: 'Anung Marganto, SH., MM.',
+    role: 'Wakil Ketua Umum III',
+    image: Koni7,
+  },
+  {
+    name: 'Heru Sarjana, S.Pd.',
+    role: 'Sekretaris Umum',
+    image: Koni21,
+  },
+  {
+    name: 'Subardi, S.Pd',
+    role: 'Wakil Sekretaris Umum',
+    image: Koni14,
+  },
+  {
+    name: 'H. Sukiman, S.Pd.',
+    role: 'Bendahara I',
+    image: Koni8,
+  },
+  {
+    name: 'Parinem, S.Pd',
+    role: 'Bendahara II',
+    image: Koni4,
+  },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
+      name: 'Bambang Sutrisno, S.Sos, M.Si',
+      role: 'Badan Audit Internal',
       image: Koni1,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
+      name: 'Barkah Hari Wibowo, SE',
+      role: 'Badan Audit Internal',
       image: Koni2,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
-      image: Koni3,
+      name: 'Sriyono, S.Pd',
+      role: 'Bidang Pembinaan Organisasi',
+      image: Koni26,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
-      image: Koni4,
+      name: 'Ferdinand Kuahati',
+      role: 'Bidang Pembinaan Organisasi',
+      image: Koni27,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
+      name: 'Hendri Utomo, SH',
+      role: 'Bidang Hukum',
+      image: Koni15,
+    },
+    {
+      name: 'Sukismadi, S.Pd',
+      role: 'Bidang BINPRES',
+      image: Koni13,
+    },
+    {
+      name: 'Tirto Mulyono',
+      role: 'Bidang BINPRES',
+      image: Koni11,
+    },
+    {
+      name: 'Marhaban',
+      role: 'Bidang Pendidikan Pelatihan dan Penataran',
+      image: Koni20,
+    },
+    {
+      name: 'Andreas Subarjo',
+      role: 'Bidang Pendidikan Pelatihan dan Penataran',
       image: Koni5,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
-      image: Koni6,
+      name: 'Romphis Oka Dwi Jaya, S.Pd',
+      role: 'Bidang IPTEK dan LITBANG Olahraga',
+      image: Koni25,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
-      image: Koni7,
+      name: 'Jumantoro, SE',
+      role: 'Bidang Rencana Program Anggaran',
+      image: Koni24,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
-      image: Koni8,
+      name: 'H. Kuswadi, ST',
+      role: 'Bidang Rencana Program Anggaran',
+      image: Koni23,
     },
     {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
+      name: 'Agnes Kristini, S.Pd, Jas',
+      role: 'Bidang Sarpras',
+      image: Koni3,
+    },
+    {
+      name: 'Parjiyono',
+      role: 'Bidang SARPRAS',
+      image: Koni17,
+    },
+    {
+      name: 'Siti Suyatimah',
+      role: 'Bidang SDM',
+      image: Koni19,
+    },
+    {
+      name: 'Drs. Raden Molyartono',
+      role: 'Bidang SDM',
       image: Koni9,
+    },
+    {
+      name: 'Aprilia Dewantoro',
+      role: 'Bidang HUMAS',
+      image: Koni12,
+    },
+    {
+      name: 'Dra. Rinawati, M.Pd',
+      role: 'Bidang Pengumpulan dan Pengolahan Data',
+      image: Koni18,
     },
     {
       name: 'Pabo Bernando',
       role: 'Web Developer',
       image: Koni10,
     },
-    {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
-      image: Koni11,
-    },
-    {
-      name: 'Pabo Bernando',
-      role: 'Web Developer',
-      image: Koni12,
-    },
-    {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni13,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni14,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni15,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni16,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni17,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni18,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni19,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni20,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni21,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni22,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni23,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni24,
-      },
-      {
-        name: 'Pabo Bernando',
-        role: 'Web Developer',
-        image: Koni25,
-      },
     
   ];
