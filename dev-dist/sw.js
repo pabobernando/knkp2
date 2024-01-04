f56342176c61c544c4129e241bb75a0211cf8e85
pabo bernando <pabobernando@desolator.local>
2024-01-04 16:01:37 +0700

    update style

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6si5atjl0ug"
+    "revision": "0.c3lmguoqvq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/VisiMisi.jsx
+++ src/components/VisiMisi.jsx
@@ -22,13 +22,10 @@ function VisiMisi() {
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 text-red-500">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
 </svg>
-
-
         <h2 className="mt-4 text-md font-bold text-white">
         Mewujudkan manajemen organisasi dan tata kelola KONI Kulon Progo yang bersih dan akuntabel
         </h2>
       </div>
-
       <div
         className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-500/10 hover:shadow-red-500/10"
       >
@@ -40,7 +37,6 @@ function VisiMisi() {
         Mengembangkan pembinaan dan pelatihan olahraga prestasi yang baik, sistematis, berjenjang dan berkesinambungan
         </h2>
       </div>
-
       <div
         className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-red-500/10 hover:shadow-red-500/10"
       >
